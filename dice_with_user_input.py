11d9c37d375c682ba043c3f2c5e3edd1027b2efb
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-01-05 05:52:19 -0800

    expanded rolling dice game with requirement for number of user

---
+++ dice_with_user_input.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import random
+
+
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+    
+    return roll
+while True:
+    players = input("Enter the number of players (1-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 1 <= players <= 4:
+            break
+        else:
+            print( "Must be between 2 to 4 players")
+    else:
+        print("Invalid kindly try again")
