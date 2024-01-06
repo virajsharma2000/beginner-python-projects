f0cf705ebce6e54d9f36c627bc7c723d424579ef
Viraj Sharma <virajsharma1501@gmail.com>
2024-01-06 10:21:20 +0530

    Just added loop to continue playing

--- dice.py
+++ dice.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 import random
 
-
 def roll():
     min_value = 1
     max_value = 6
+
     roll = random.randint(min_value, max_value)
     
     return roll
 
-value = roll()
-print(value)
 
+while True:
+ key = input('Enter C to continue, E to exit: ')
+
+ if key.capitalize() == 'C':
+  value = roll()
+
+  print(value)
+
+ elif key.capitalize() == 'E':
+  print('thank you for playing')
+  break
+
+ else:
+  print('Unknown letter')
